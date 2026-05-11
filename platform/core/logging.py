0499ec64b032ed94e298385f603a41f56e0b42fe
hellodk <hello.dk@outlook.com>
2026-05-11 14:05:39 +0530

    feat: structlog 24 JSON logging

---
+++ platform/core/logging.py
@@ -0,0 +1,32 @@
+import logging
+import structlog
+
+
+def configure_logging(level: str = "INFO") -> None:
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(
+            logging.getLevelName(level)
+        ),
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.getLevelName(level),
+    )
+
+
+def get_logger(name: str = __name__):
+    return structlog.get_logger(name)
